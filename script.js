1e7a908b0e068f0ffb99d5027b7677c644e124e7
dojae-bora <bora03060306@naver.com>
2026-07-08 14:34:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -123,12 +123,54 @@ hoursEl.innerText = String(hours).padStart(2, "0");
 minEl.innerText = String(minutes).padStart(2, "0");
 secEl.innerText = String(seconds).padStart(2, "0");
 }
-function openModal(src) {
-  const modal = document.getElementById("image-modal");
-  const modalImg = document.getElementById("modal-img");
-  if (!modal || !modalImg) return;
-  modal.style.display = "flex";
-  modalImg.src = src;
+
+let currentIndex = 0;
+
+const galleryImages = [
+  "images/photo1.jpg",
+  "images/photo2.jpg",
+  "images/photo3.jpg",
+  "images/photo4.jpg",
+  "images/photo5.jpg",
+  "images/photo6.jpg",
+  "images/photo7.jpg",
+  "images/photo8.jpg",
+  "images/photo9.jpg",
+  "images/photo10.jpg",
+  "images/photo11.jpg",
+  "images/photo12.jpg",
+  "images/photo13.jpg",
+  "images/photo14.jpg",
+  "images/photo15.jpg",
+  "images/photo16.jpg",
+  "images/photo17.jpg",
+  "images/photo18.jpg",
+  "images/photo19.jpg",
+  "images/photo20.jpg",
+  "images/photo21.jpg",
+  "images/photo22.jpg",
+  "images/photo23.jpg",
+  "images/photo24.jpg",
+  "images/photo25.jpg",
+  "images/photo26.jpg",
+  "images/photo27.jpg",
+  "images/photo28.jpg",
+  "images/photo29.jpg",
+  "images/photo30.jpg",
+  "images/photo31.jpg",
+  "images/photo32.jpg"
+];
+
+function openModal(index){
+
+    currentIndex = index;
+
+    const modal = document.getElementById("image-modal");
+    const modalImg = document.getElementById("modal-img");
+
+    modalImg.src = galleryImages[currentIndex];
+
+    modal.style.display = "flex";
 }
 function closeModal() {
   const modal = document.getElementById("image-modal");
