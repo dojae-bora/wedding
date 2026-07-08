36c82bad1dd45c8467c72a9da968069b95cb0730
dojae-bora <bora03060306@naver.com>
2026-07-08 17:08:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -169,6 +169,9 @@ function openModal(index){
 
     modalImg.src = galleryImages[currentIndex];
 
+  document.getElementById("modal-counter").innerText =
+  (currentIndex + 1) + " / " + galleryImages.length;
+
     modal.style.display = "flex";
 
   document.body.style.overflow = "hidden";
@@ -182,17 +185,19 @@ function closeModal() {
   }
 }
 function changePhoto(direction) {
+ 
   currentIndex = currentIndex + direction;
 
   if (currentIndex < 0) {
     currentIndex = galleryImages.length - 1;
   }
-
   if (currentIndex >= galleryImages.length) {
     currentIndex = 0;
   }
-
   document.getElementById("modal-img").src = galleryImages[currentIndex];
+  }
+  document.getElementById("modal-counter").innerText =
+  (currentIndex + 1) + " / " + galleryImages.length;
 }
 
 let startX = 0;
